2c0964aa172627a7e498e5e6ff657cbdcf0b7554
Tejas <62064506+KidCoderT@users.noreply.github.com>
2021-11-28 11:33:01 +0530

    Some Changes

--- main.py
+++ main.py
@@ -1,35 +1,35 @@
-	import sys
-	import pygame
+import sys
+import pygame
 
-	pygame.init()
-	pygame.mixer.init()
+pygame.init()
+pygame.mixer.init()
 
-	from game import *
-	from screens import death_screen, main_menu_screen
+from game import *
+from screens import death_screen, main_menu_screen
 
-	screen = pygame.display.set_mode((SCREEN_WIDTH * 1.1, SCREEN_HEIGHT * 1.1))
-	pygame.display.set_caption(game_name)
-	pygame.display.set_icon(pygame.image.load("assets/icon.ico"))
+screen = pygame.display.set_mode((SCREEN_WIDTH * 1.1, SCREEN_HEIGHT * 1.1))
+pygame.display.set_caption(game_name)
+pygame.display.set_icon(pygame.image.load("assets/icon.ico"))
 
-	should_replay = False
-	while True:
-		if not should_replay:
-			can_play_game = main_menu_screen(screen)
-			if not can_play_game:
-				break
-			elif can_play_game == "howtoplay":
-				print("okay")
-		else:
-			should_replay = False
-
-		trashes_collected = run_game(screen)
-		should_continue = death_screen(screen, 55)
-
-		if not should_continue:
+should_replay = False
+while True:
+	if not should_replay:
+		can_play_game = main_menu_screen(screen)
+		if not can_play_game:
 			break
-		
-		if should_continue == "replay":
-			should_replay = True
+		elif can_play_game == "howtoplay":
+			print("okay")
+	else:
+		should_replay = False
 
-	pygame.quit()
-	sys.exit()
+	trashes_collected = run_game(screen)
+	should_continue = death_screen(screen, 55)
+
+	if not should_continue:
+		break
+	
+	if should_continue == "replay":
+		should_replay = True
+
+pygame.quit()
+sys.exit()
